2576811c5877c75c2f0a37a150743c309811781f
richie <richie.kwon1@gmail.com>
2021-12-07 21:51:29 +0000

    button

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,21 +1,28 @@
 import "./App.css";
 import React, {useState} from 'react'
 
-const data = [
-  { title: "node", value: 0 },
-  { title: "react", value: 1 },
-];
+// const data = [
+//   { title: "node", value: 0 },
+//   { title: "react", value: 1 },
+// ];
+
+
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
+      
+      <header >
+        <button onClick={()=>setCount(count+1)}>{count}</button>
+      </header>
+      {/* <header className="App-header">
         {data.map((item) => (
           <>
             <p key={item.value}>{item.title}, {item.value}</p>
           </>
         ))}
-      </header>
+      </header> */}
     </div>
   );
 }
