ac3196bf6e19206765b1464badefab61858a28c8
richie <richie.kwon1@gmail.com>
2021-12-10 22:57:09 +0000

    minw

--- redux/src/App copy.js
+++ redux/src/App copy.js
@@ -10,7 +10,21 @@ function App() {
   
   return (
     <div className="App">
-    
+      <header>
+        <button onClick={() => setCount(count + 1)}>{count}</button>
+      </header>
+
+      <div className="todolist">
+
+        <Todo></Todo>
+      </div>
+      {/* <header className="App-header">
+        {data.map((item) => (
+          <>
+            <p key={item.value}>{item.title}, {item.value}</p>
+          </>
+        ))}
+      </header> */}
     </div>
   );
 }
