62e93aea2fc980041ea43c79f014b4f9f54dfc7b
richie <richie.kwon1@gmail.com>
2021-12-13 22:01:17 +0000

    flux

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,18 +1,40 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-
+import axios from 'axios'
 // const data = [
 //   { title: "node", value: 0 },
 //   { title: "react", value: 1 },
 // ];
 
 function App() {
-  
-  
+
+  const [data, setData] = useState({ hits: [] })
+  const [query, setQuery] = useState('react')
+
+  useEffect(() => {
+    let completed = false
+
+    async function get() {
+      const result = await axios(`https://hn.algolia.com/api/v1/search?query=${query}`)
+      if (!completed) setData(result.data)
+    }
+    get()
+    return () => { 
+      completed = true  
+    }
+  }, [query])
   return (
-    <div className="App">
+    <>
       
-    </div>
+        <input value={query} onChange={e => setQuery(e.target.value)} />
+        <ul>
+          {data.hits.map(item =>(
+            <li key={item.objectId}>
+              <a href={item.url}>{item.title}</a>
+            </li>))}
+        </ul>
+      
+    </>
   );
 }
 
